98f5c6310d11831b05e69f1753963a2dada4533a
Khrizelle28 <sablankhrizelle@gmail.com>
2025-11-23 12:32:14 +0800

    Fix mail directory case sensitivity

---
+++ app/temp_mail/InvoiceMail.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class InvoiceMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $data;
+
+    public $type;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($data, string $type)
+    {
+        $this->data = $data;
+        $this->type = $type; // 'appointment' or 'order'
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        $subject = $this->type === 'appointment'
+            ? 'Payment Invoice - Appointment Confirmation'
+            : 'Payment Invoice - Order Confirmation';
+
+        return new Envelope(
+            subject: $subject,
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        $view = $this->type === 'appointment'
+            ? 'emails.invoice-appointment'
+            : 'emails.invoice-order';
+
+        return new Content(
+            view: $view,
+            with: [
+                'data' => $this->data,
+            ],
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
